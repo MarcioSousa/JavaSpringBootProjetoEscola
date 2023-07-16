07f6491ca54e47bc341a4008aad1e07f37f308af
Marcio Sousa <marciofrapeso@gmail.com>
2023-07-16 20:23:02 -0300

    Criado classe Curso.

---
+++ src/main/java/com/cad/escola/Curso.java
@@ -0,0 +1,76 @@
+package com.cad.escola;
+
+public class Curso {
+	private String Sigla;
+	private String Nome;
+	private int CargaHoraria;
+	private double Valor;
+	private String DataVigencia;
+	private double ValorHoraInstrutor;
+	private String Programa;
+	
+	public Curso(String sigla, String nome) {
+		super();
+		Sigla = sigla;
+		Nome = nome;
+	}
+	
+
+	public String getSigla() {
+		return Sigla;
+	}
+
+
+	public void setSigla(String sigla) {
+		Sigla = sigla;
+	}
+	
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public int getCargaHoraria() {
+		return CargaHoraria;
+	}
+
+	public double getValor() {
+		return Valor;
+	}
+
+	public String getDataVigencia() {
+		return DataVigencia;
+	}
+
+	public double getValorHoraInstrutor() {
+		return ValorHoraInstrutor;
+	}
+
+	public String getPrograma() {
+		return Programa;
+	}
+
+	public void setCargaHoraria(int cargaHoraria) {
+		CargaHoraria = cargaHoraria;
+	}
+	
+	public void setValor(double valor) {
+		Valor = valor;
+	}
+
+	public void setDataVigencia(String dataVigencia) {
+		DataVigencia = dataVigencia;
+	}
+	
+	public void setValorHoraInstrutor(double valorHoraInstrutor) {
+		ValorHoraInstrutor = valorHoraInstrutor;
+	}
+	
+	public void setPrograma(String programa) {
+		Programa = programa;
+	}
+	
+}
--- src/main/java/com/cad/escola/EscolaApplication.java
+++ src/main/java/com/cad/escola/EscolaApplication.java
@@ -7,7 +7,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EscolaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EscolaApplication.class, args);
+		//SpringApplication.run(EscolaApplication.class, args);
+		
+		Curso curso1 = new Curso("AAA","Analista de Dados");
+		curso1.setCargaHoraria(30);
+		curso1.setValor(1.30);
+		curso1.setDataVigencia("10/02/2023");
+		curso1.setCargaHoraria(30);
+		curso1.setPrograma("Gratuito");
+		
+		Curso curso2 = new Curso("BBB","Engenharia");
+		curso2.setCargaHoraria(40);
+		curso2.setValor(2.10);
+		curso2.setDataVigencia("10/03/2023");
+		curso2.setCargaHoraria(40);
+		curso2.setPrograma("Pago");
+
+		System.out.println("O valor do curso e: " + curso1.getValor());
+		System.out.println("A data vigencia e: " + curso1.getDataVigencia());
+		System.out.println(curso1.getCargaHoraria());
+		System.out.println(curso1.getNome());
+		System.out.println(curso1.getPrograma());
+		
+		
+		System.out.println("-----------------------------");
+		
+		
+		System.out.println(curso2.getValor());
+		System.out.println(curso2.getDataVigencia());
+		System.out.println(curso2.getCargaHoraria());
+		System.out.println(curso2.getNome());
+		System.out.println(curso2.getPrograma());
+		
 	}
 
 }
